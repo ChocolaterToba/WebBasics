270fe43284c85c87c53b070f7959dfc221ec8e6e
Mikhail Aglicheev <toba.ru@mail.ru>
2020-11-17 01:59:10 +0300

    Started fixing filldb function

--- app/management/commands/filldb.py
+++ app/management/commands/filldb.py
@@ -30,33 +30,32 @@ class Command(BaseCommand):
         answers_cnt = kwargs['answers']
 
         if users_cnt:
-            print('====================')
-            self.fill_users(users_cnt)
-            print('------------------------')
             self.fill_profiles(users_cnt)
-            print('++++++++++++++++++++++++')
             if questions_cnt:
                self.fill_questions(questions_cnt)
 
     def fill_users(self, cnt):
         for i in range(cnt):
             User.objects.create_user(
-                f.first_name(),
-                f.email(),
-                f.password(length=f.random_int(min=8, max=12)))
+                f.unique.first_name(),
+                f.unique.email(),
+                f.password(length=f.random_int(min=8, max=12))
+            )
         
     def fill_profiles(self, cnt):
-        user_ids = list(
+        initial_users_amount = User.objects.count()
+        self.fill_users(cnt)
+        user_ids = sorted(list(
             User.objects.values_list(
                 'id', flat=True
             )
-        )
+        ))[initial_users_amount:]
 
         avatars_path = 'uploads/avatars/'
         avatar_links = [f for f in listdir(avatars_path) if isfile(join(avatars_path, f))]
         for i in range(cnt):
             Profile.objects.create(
-                user=user_ids[i],
+                user_id=user_ids[i],
                 avatar='avatars/' + choice(avatar_links),
                 nickname=f.name()[:31]
             )
@@ -73,12 +72,14 @@ class Command(BaseCommand):
             )
         )
         for i in range(cnt):
+            print(i)
             Question.objects.create(
-                author=choice(profile_ids),
+                author_id=choice(profile_ids),
                 title=f.sentence(nb_words=5)[:256],
                 text='. '.join(f.sentences(f.random_int(min=2, max=7))),
-                publishing_date=f.date_between('1970.01.01', 'today'),
-                tags=choices(tag_names, k=f.random_int(min=0, max=5)),
-                likes=choices(profile_ids, k=f.random_int(min=0, max=len(profile_ids)))
+                publishing_date=f.date_between('-40y', 'today'),
             )
+            Question.objects.last().tags.set(choices(tag_names, k=f.random_int(min=0, max=5)))
+            Question.objects.last().likes.set(choices(profile_ids, k=f.random_int(min=0, max=len(profile_ids))),
+                                              through_defaults={'is_a_like': True)
             Question.objects.last().RefreshRating()
