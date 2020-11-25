43216db1cffc543c6102d19b3aafdb1638c217a1
Mikhail Aglicheev <toba.ru@mail.ru>
2020-11-25 20:15:58 +0300

    Added bootstrap to login form

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,20 @@ from django import forms
 from app.models import Question
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(max_length=150, 
+        widget=forms.TextInput(attrs={
+            'class': 'right-col form-control col-sm-6',
+            'placeholder': 'Enter your Username here',
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'right-col form-control col-sm-6',
+            'placeholder': '********',
+            }
+        )
+    )
 
 class AskForm(forms.ModelForm):
     class Meta:
